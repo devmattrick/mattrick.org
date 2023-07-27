55a4390df8388ca3fea152be25e881e05fc6edba
Matthew McCune <matt@mattrick.org>
2023-07-26 23:15:17 -0700

    More work on design

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://mattrick.org",
   integrations: [sitemap()],
+  experimental: {
+    assets: true,
+  },
 });
--- src/content/posts/test.md
+++ src/content/posts/test.md
@@ -1,6 +1,8 @@
 ---
 title: Test
-draft: true
+description: This is a test post.
+date: 7/26/2023 00:00
+draft: false
 ---
 
 # Test
