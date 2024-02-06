8ef0e5e2976420d4e7315148a089b19c6c6b7f85
Matthew McCune <matt@mattrick.org>
2024-02-06 01:45:16 -0800

    Begin migration to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
-import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://mattrick.org",
-  integrations: [sitemap()],
-  experimental: {
-    assets: true,
-  },
-});
+export default defineConfig({});
