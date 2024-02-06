152fb6d9e6a1a90f4eae590087d8de7f6adbace3
Matthew McCune <matt@mattrick.org>
2024-02-06 02:04:45 -0800

    Linting and formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+const config = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+};
+
+module.exports = config;
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mattrick.org"
+  site: "https://mattrick.org",
 });
